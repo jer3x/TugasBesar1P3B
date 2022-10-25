29201928d1dd14f39b1ccd68b91c921734242d8f
jer <jeremychristian2323@gmail.com>
2022-10-25 22:20:51 +0700

    coba commit7

--- app/src/main/java/com/example/tb/MainActivity.java
+++ app/src/main/java/com/example/tb/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.tb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.tugasbesar.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     private datePickerFragment datePickerFragment;
+    private timePickerFragment timePickerFragment;
     private FragmentManager fragmentManager;
     private MainFragment mainFragment;
-
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +30,32 @@ public class MainActivity extends AppCompatActivity {
 
         //2. Instantiate the Fragment in Activity
         this.datePickerFragment = datePickerFragment.newInstance();
-
+        this.timePickerFragment = timePickerFragment.newInstance();
         this.fragmentManager = this.getSupportFragmentManager();
 
         //3. Instantiate FragmentManager
         this.fragmentManager = this.getSupportFragmentManager();
 
-        //4. Use Fragment transaction
-        FragmentTransaction ft = this.fragmentManager.beginTransaction();
-        ft.add(R.id.fragment_container, this.mainFragment).addToBackStack(null).commit();
+//        //4. Use Fragment transaction
+//        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+//        ft.add(R.id.fragment_container, this.mainFragment).addToBackStack(null).commit();
+
+
+        this.textView = findViewById(R.id.tv_waktu);
+        this.button = findViewById(R.id.button_Waktu);
+
+        button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 DialogFragment timePicker = new timePickerFragment();
+                 timePicker.show(getSupportFragmentManager(), "time picker");
+             }
+         });
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        textView.setText("Hour: " + hourOfDay + " Minute" + minute);
     }
 }
--- app/src/main/java/com/example/tb/timePickerFragment.java
+++ app/src/main/java/com/example/tb/timePickerFragment.java
@@ -1,5 +1,54 @@
 package com.example.tb;
 
-public class timePickerFragment {
+import static android.content.ContentValues.TAG;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import com.example.tugasbesar.R;
+
+import java.util.Calendar;
+
+public class timePickerFragment extends DialogFragment {
+
+    private TextView textView;
+    private EditText editText;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle saveInstanceState){
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+
+        return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+
+
+
+
+    public timePickerFragment(){
+        //Required empty public constuctor
+
+    }
+
+
+    public timePickerFragment newInstance() {
+        timePickerFragment fragment = new timePickerFragment();
+        return fragment;
+    }
 }
